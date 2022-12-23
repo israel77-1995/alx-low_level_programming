99a9a99a2fc37c8222a024dc56a376dd936aa0f6
israel77-1995 <israelmbuyu77@gmail.com>
2022-12-23 02:48:08 -0800

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,15 +3,11 @@
 /**
  * main - will print a text
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 
-	printf("\ "with proper grammar, but the outcome is a piece of
-
-			art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
