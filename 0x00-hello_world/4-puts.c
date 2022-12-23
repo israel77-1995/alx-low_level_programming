6a3a2102d53d0cf69baa4995c43e7aaec57c7fed
israel77-1995 <israelmbuyu77@gmail.com>
2022-12-23 02:33:21 -0800

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include < stdio.h >
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -7,13 +7,9 @@ include < stdio.h >
  */
 
 int main(void)
+ {
 
-{
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
-}
+ }
