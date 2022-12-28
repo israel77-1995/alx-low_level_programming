34357d5d9a5bba7e324a70a1c30133e6889243c2
israel77-1995 <israelmbuyu77@gmail.com>
2022-12-28 02:35:30 -0800

    aplha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints the alphabet
+ * main - entry
  *
- * Return: Always 0 (Success)
+ * return - always 0 (success)
  */
+
 int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a';low <= 'z'; low++)
 		putchar(low);
 		putchar('\n');
 
 	return (0);
 }
+
