029ff221079512ff74cdefbfa97e1b0f369aab93
Israel Mbuyu <israelmbuyu77@gmail.com>
2023-04-20 04:35:56 -0700

    printing string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print string recursively
+ * @s: parameter for string
+ */
+
+void _puts_recursion(char *s)
+{
+	s = "Israel"
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+         putchar(*s)
+	 s++;
+	 0-puts_recursion.c(s)
+}
+
