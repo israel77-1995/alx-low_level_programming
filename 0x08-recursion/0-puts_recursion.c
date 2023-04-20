ec2d6e47e5216d9c3548a040a7a508d1771b925a
Israel Mbuyu <israelmbuyu77@gmail.com>
2023-04-20 12:44:17 -0700

    checj

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,16 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
-}
-
-int main()
-{
-	return 0;
+	if (*s == '\0')
+	 {
+		  _putchar.c('\n');
+		  return;
+	 }
+	_putchar(*s);
+	s++
+	_putchar(s);
 }
