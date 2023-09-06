717fd5f264653268f1778a714c9faa4b8ab0f4ce
israel77-1995 <israelmbuyu77@gmail.com>
2023-09-06 12:54:56 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 	else
-		_putchar(*s);
-		_puts_recursion(s + 1);
-
+		_putchar('\n');
 }
+
 
